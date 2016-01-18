a287b7f8d6ff6433bdd458b9e90246dddad6aea2
KaMe <julioramon767@gmail.com>
2016-01-18 13:37:31 -0400

    Update welcome_index_lang.php

--- application/language/spanish/app_files/welcome_index_lang.php
+++ application/language/spanish/app_files/welcome_index_lang.php
@@ -1,2 +1,2 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
-$lang['homepage_welcome'] = 'Bienvenido a tu sitio web. Estamos seguros que te gustar&oacute; lo que encontraras aqu&ioacute;.';
+$lang['homepage_welcome'] = 'Bienvenido a tu sitio web. Estamos seguros que te gustar&iacute; lo que encontraras aqu&iacute;.';
