be5f68b9591388903a0fbda970d1c8272c67e58a
Adrian Voicu <avenir.ro@gmail.com>
2016-06-13 13:08:53 +0300

    use db for session

--- application/config/config.php
+++ application/config/config.php
@@ -354,10 +354,10 @@ $config['encryption_key'] = '';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'files';
+$config['sess_driver'] = 'database';
 $config['sess_cookie_name'] = 'ciapp_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = APPPATH.'session';
+$config['sess_save_path'] = 'ci_sessions';
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
