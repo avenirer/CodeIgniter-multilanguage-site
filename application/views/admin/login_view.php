c53efc549f1e2ecf9f4e69e8771eda1bf4a35960
Avenirer <avenir.ro@gmail.com>
2017-02-21 10:56:34 +0200

    ...
    …

--- application/views/admin/login_view.php
+++ application/views/admin/login_view.php
@@ -1,5 +1,5 @@
 <div class="row">
-    <div class="col-lg-4 col-lg-offset-4">
+    <div class="col-xs-10 col-xs-offset-1 ol-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4 col-lg-2 col-lg-offset-5">
         <h1>Login</h1>
         <?php
         echo $this->postal->get();
@@ -20,7 +20,9 @@
                 <?php echo form_checkbox('remember','1',FALSE);?> Remember me
             </label>
         </div>
-        <?php echo form_submit('submit', 'Log in', 'class="btn btn-primary btn-lg btn-block"');?>
+        <div class="form-group">
+            <?php echo form_submit('submit', 'Log in', 'class="btn btn-primary btn-lg btn-block"');?>
+        </div>
         <?php echo form_close();?>
     </div>
 </div>
